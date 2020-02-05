6e25bdf61ef3e8a9626ad2cb9e3170ae0c5eff8a
Lucas Maciel <lucas333maciel@gmail.com>
2020-02-05 00:51:57 -0300

    config 1.5

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -17,9 +17,29 @@ class UserController {
     });
   }
 
-  update(req, res) {
-    console.log(req.userId);
-    return res.json({ ok: 'OK' });
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExist = await User.findOne({ where: { email } });
+
+      if (userExist) {
+        return res.status(400).json({ error: 'user alredy exist' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
