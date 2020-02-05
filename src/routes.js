a647eef8fee71bc176a1a68d8d1960549be2c390
Lucas Maciel <lucas333maciel@gmail.com>
2020-02-04 22:00:12 -0300

    config 1.1

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Lucas Maciel',
-    email: 'lucas333maciel@gmail.com',
-    password_hash: '154844',
-  });
-  res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
