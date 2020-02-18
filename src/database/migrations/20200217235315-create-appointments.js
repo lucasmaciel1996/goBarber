f13634321282fb958ee24b93bf0253b14ba6e6d1
Lucas Maciel <lucas333maciel@gmail.com>
2020-02-17 23:06:07 -0300

    add agenda

---
+++ src/database/migrations/20200217235315-create-appointments.js
@@ -0,0 +1,44 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('appointments', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: { model: 'users', key: 'id' },
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      },
+      provider_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: { model: 'users', key: 'id' },
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      },
+      calceled_at: {
+        type: Sequelize.DATE,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+  down: queryInterface => {
+    return queryInterface.dropTable('appointments');
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentsController from './app/controllers/AppointmentController';
 import middlewaresAuth from './app/middlewares/auth';
 
 const routes = new Router();
@@ -16,6 +17,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(middlewaresAuth);
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
+routes.post('/appointments', AppointmentsController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
