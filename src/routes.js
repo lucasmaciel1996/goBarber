01882a1b1162f561bb34b9d8af086ea7388a4ccb
Lucas Maciel <lucas333maciel@gmail.com>
2020-02-19 22:00:49 -0300

    add shedule provider

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentsController from './app/controllers/AppointmentController';
+import SheduleController from './app/controllers/SheduleController';
 import middlewaresAuth from './app/middlewares/auth';
 
 const routes = new Router();
@@ -19,6 +20,8 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.get('/appointments', AppointmentsController.index);
 routes.post('/appointments', AppointmentsController.store);
+
+routes.get('/shedule', SheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
