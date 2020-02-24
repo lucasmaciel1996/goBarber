4e6bf60fc371e61a6a59783607ce67241eec62de
Lucas Maciel <lucas333maciel@gmail.com>
2020-02-24 15:01:32 -0300

    update notification

--- src/routes.js
+++ src/routes.js
@@ -24,6 +24,7 @@ routes.post('/appointments', AppointmentsController.store);
 
 routes.get('/shedule', SheduleController.index);
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
