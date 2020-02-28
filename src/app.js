0d3e7896921c2110875aee488368813b9ff8379a
Lucas Maciel <lucas333maciel@gmail.com>
2020-02-28 17:45:20 -0300

    add check erros request

--- src/app.js
+++ src/app.js
@@ -1,17 +1,28 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+
+import 'express-async-errors';
+
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandle();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -21,6 +32,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandle() {
+    this.server.use(async (erro, req, res, next) => {
+      const erros = await new Youch(erro, req).toJSON();
+
+      return res.status(500).json(erros);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://209dee9de8314e7da20f3c64e45bee82@sentry.io/3161507',
+};
