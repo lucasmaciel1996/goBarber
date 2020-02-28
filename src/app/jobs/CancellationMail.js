47d497539f9acbf0430d5609de9290da74f2d601
Lucas Maciel <lucas333maciel@gmail.com>
2020-02-28 20:25:51 -0300

    log exc fila

--- src/app/jobs/CancellationMail.js
+++ src/app/jobs/CancellationMail.js
@@ -9,6 +9,7 @@ class CancellationMail {
 
   async handle({ data }) {
     const { appointment } = data;
+    console.log('A fila executou!!');
 
     await Mail.sendEmail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
