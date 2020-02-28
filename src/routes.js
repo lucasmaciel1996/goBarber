c86585bc73a2d86b6e1270d2a6dcfcf61697bfdf
Lucas Maciel <lucas333maciel@gmail.com>
2020-02-28 16:38:46 -0300

    available shedule

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentsController from './app/controllers/AppointmentController';
 import SheduleController from './app/controllers/SheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 import middlewaresAuth from './app/middlewares/auth';
 
 const routes = new Router();
@@ -18,7 +19,10 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(middlewaresAuth);
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
 routes.get('/appointments', AppointmentsController.index);
 routes.post('/appointments', AppointmentsController.store);
 routes.delete('/appointments/:id', AppointmentsController.delete);
